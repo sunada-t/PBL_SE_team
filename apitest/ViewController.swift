099ff425fd9111a29837563859c79c0ee7143cc6
hiromu-m <v010maaa@gmail.com>
2019-12-17 22:13:32 +0900

    Initial Commit

---
+++ apitest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apitest
+//
+//  Created by hiromu-m on 2019/12/17.
+//  Copyright © 2019 hiromu-m. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
