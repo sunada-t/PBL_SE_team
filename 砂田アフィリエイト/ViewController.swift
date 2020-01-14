093351ee330c8f83a66f284fdeb8df1b83cf0a19
hiromu-m <v010maaa@gmail.com>
2020-01-14 14:08:48 +0900

    Initial Commit

---
+++ 砂田アフィリエイト/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  砂田アフィリエイト
+//
+//  Created by hiromu-m on 2020/01/14.
+//  Copyright © 2020 hiromu-m. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
