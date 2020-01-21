02593ef945f238f23bc7b09d5532dcbd573d8825
hiromu-m <v010maaa@gmail.com>
2020-01-21 16:06:57 +0900

    api

---
+++ apitest/NaistWebViewController.swift
@@ -0,0 +1,71 @@
+//
+//  NaistWebViewController.swift
+//  apitest
+//
+//  Created by hiromu-m on 2020/01/21.
+//  Copyright © 2020 hiromu-m. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import Alamofire
+import Kanna
+
+class NaistWebViewController: UIViewController,WKNavigationDelegate{
+
+    
+    @IBOutlet weak var webview: WKWebView!
+    
+    let params: Parameters = [
+        "Username": "ryulstory",
+        "Password": "1234!"
+    ]
+
+    let loginRequest = [
+            "data[User][account]": "aaaaaaa",
+            "data[User][password]": "UserPassword"
+        ]
+    let headers = ["Content-Type" : "text/html"]
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        if let url = URL(string: "https://subjregist.naist.jp"){
+        self.webview.load(URLRequest(url: url))
+          // Do any additional setup after loading the view.
+          print("-----------")
+          print(self.webview.url!)
+          print("-----------")
+            
+            Alamofire.request(url, method : .post, parameters : loginRequest, encoding :
+            JSONEncoding.default , headers : headers).responseData { dataResponse in
+
+            print("[[[[[[[[[[[[[[[[[[[[")
+             print(dataResponse.request as Any) // your request
+             print(dataResponser.response as Any) // your response
+                print("]]]]]]]]]]]]]]]")
+             }
+            
+          }
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- apitest/ViewController.swift
+++ apitest/ViewController.swift
@@ -5,16 +5,90 @@
 //  Created by hiromu-m on 2019/12/17.
 //  Copyright © 2019 hiromu-m. All rights reserved.
 //
+//Article(title: "GKE中のGolangアプリケーションからCloud Pub/Subを使ってデータ連携を行う", user: apitest.Article.User(id: "devs_hd"))
 
 import UIKit
 
+
+struct Article: Codable {
+    var title: String
+    var user: User
+    struct User: Codable {
+        var id: String
+    }
+}
+
+struct Qiita {
+
+//情報取得
+    static func fetchArticle(completion: @escaping ([Article]) -> Swift.Void) {
+//get ur
+        let url = "https://qiita.com/api/v2/items"
+        print(url)
+        //クエリストリング
+        guard var urlComponents = URLComponents(string: url) else {
+            return
+        }
+//構造体
+//表示数５０perpageデフォルト
+        urlComponents.queryItems = [
+            URLQueryItem(name: "per_page", value: "50"),
+        ]
+      
+        let task = URLSession.shared.dataTask(with: urlComponents.url!) { data, response, error in
+
+            guard let jsonData = data else {
+                return
+            }
+
+            do {
+                let articles = try JSONDecoder().decode([Article].self, from: jsonData)
+                completion(articles)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+}
+
+
 class ViewController: UIViewController {
+
+    private var tableView = UITableView()
+    fileprivate var articles: [Article] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "最新記事"
+
+        setUpTableView: do {
+            tableView.frame = view.frame
+            tableView.dataSource = self
+            view.addSubview(tableView)
+        }
+
+        Qiita.fetchArticle(completion: { (articles) in
+            self.articles = articles
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        })
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//cellを作っている
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article.title
+        cell.detailTextLabel?.text = article.user.id
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+}
