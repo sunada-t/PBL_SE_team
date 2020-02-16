cb619f4369399a82832545fbe5002ed81790729a
hiromu-m <v010maaa@gmail.com>
2020-02-17 02:07:44 +0900

    ko-do変更

--- apitest/NaistWebViewController.swift
+++ apitest/NaistWebViewController.swift
@@ -24,49 +24,67 @@ class NaistWebViewController: UIViewController{
     @IBOutlet weak var ipp: UILabel!
     @IBOutlet weak var sen: UILabel!
     @IBOutlet weak var pbll: UILabel!
-     var ippan1 = 0
-    var joron2 = 0
-    var senmonkamoku = 0
-    var pbl = 0
-    var remain = 0
+    @IBOutlet weak var assesment: UILabel!
+    
+    @IBOutlet weak var jojo: UIButton!
+
+    @IBOutlet weak var papa: UIButton!
+    @IBOutlet weak var sensen: UIButton!
+    @IBOutlet weak var pbpb: UIButton!
+    
+    var ippan1 = 0.0
+    var joron2 = 0.0
+    var senmonkamoku = 0.0
+    var pbl = 0.0
+    var remain = 0.0
     override func viewDidLoad() {
-        
+         super.viewDidLoad()
         if(Common.getUserDefaults(key: "aaa") != nil){
-            ippan1 = Int(Common.getUserDefaults(key: "aaa")as! Double)
-        
-            joron2 = Int(Common.getUserDefaults(key: "bbb")as! Double)
-        
-            senmonkamoku = Int(Common.getUserDefaults(key: "ccc")as! Double)
-        
-            pbl = Int(Common.getUserDefaults(key: "eee")as! Double)
+            ippan1 = Common.getUserDefaults(key: "aaa")as! Double
+            joron2 = Common.getUserDefaults(key: "bbb")as! Double
+            senmonkamoku = Common.getUserDefaults(key: "ccc")as! Double
+            pbl = Common.getUserDefaults(key: "eee")as! Double
         }
         
         
         
-        super.viewDidLoad()
+       
         
         joron.progress = Float(joron2/3)
         ippan.progress = Float(ippan1/4)
         sennmonkuso.progress = Float(senmonkamoku/12)
         pblb.progress = Float(pbl/2)
+        
         jo.text = "\(String(Int(joron2)))/3"
         ipp.text = "\(String(Int(ippan1)))/4"
         sen.text = "\(String(Int(senmonkamoku)))/12"
         pbll.text = "\(String(Int(pbl)))/2"
-        if(Float(joron2/3)>1){
+        
+        if(Float(joron2/3)<1){
             remain = remain + (3-joron2)
         }
-        if(Float(ippan1/4)>1){
+        if(Float(ippan1/4)<1){
             remain = remain + (4-ippan1)
         }
-        if(Float(senmonkamoku/12)>1){
+        if(Float(senmonkamoku/12)<1){
             remain = remain + (12-senmonkamoku)
         }
-        if(Float(pbl/2)>1){
+        if(Float(pbl/2)<1){
             remain = remain + (2-pbl)
         }
         
-        engurah.value = CGFloat(((21-remain)/remain)*100)
+        engurah.value = CGFloat(Float((21-remain)/21)*100)
+        
+        if(engurah.value>=90){
+            assesment.text = "後は英語だけかな？残り卒論がんばろう！"
+        }else if(engurah.value>=60){
+            assesment.text = "今が頑張りところ"
+        }else if(engurah.value>=30){
+            assesment.text = "M1前半はテストが多いががんばろう"
+        }else if(engurah.value >= 0){
+            assesment.text = "今が冬なら君は詰んでいるよ，潔く諦めよう"
+        }
+        
           }
 
 
