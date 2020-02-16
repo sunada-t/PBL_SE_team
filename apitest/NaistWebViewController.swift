2fe8dfd0cb38ffb9711fdb9d09bed9cc692ebcb0
hiromu-m <v010maaa@gmail.com>
2020-02-16 23:01:56 +0900

    割合の式を追加した．割合で表示できる

--- apitest/NaistWebViewController.swift
+++ apitest/NaistWebViewController.swift
@@ -24,26 +24,49 @@ class NaistWebViewController: UIViewController{
     @IBOutlet weak var ipp: UILabel!
     @IBOutlet weak var sen: UILabel!
     @IBOutlet weak var pbll: UILabel!
-    
-    
-    
-    
-    let ippan1 = Common.getUserDefaults(key: "aaa")as! Double
-    let joron2 = Common.getUserDefaults(key: "bbb")as! Double
-    let senmonkamoku = Common.getUserDefaults(key: "ccc")as! Double
-    let pbl = Common.getUserDefaults(key: "eee")as! Double
-    
+     var ippan1 = 0
+    var joron2 = 0
+    var senmonkamoku = 0
+    var pbl = 0
+    var remain = 0
     override func viewDidLoad() {
+        
+        if(Common.getUserDefaults(key: "aaa") != nil){
+            ippan1 = Int(Common.getUserDefaults(key: "aaa")as! Double)
+        
+            joron2 = Int(Common.getUserDefaults(key: "bbb")as! Double)
+        
+            senmonkamoku = Int(Common.getUserDefaults(key: "ccc")as! Double)
+        
+            pbl = Int(Common.getUserDefaults(key: "eee")as! Double)
+        }
+        
+        
+        
         super.viewDidLoad()
-        engurah.value = 90
+        
         joron.progress = Float(joron2/3)
         ippan.progress = Float(ippan1/4)
         sennmonkuso.progress = Float(senmonkamoku/12)
         pblb.progress = Float(pbl/2)
-        jo.text = String(Int(joron2))
-        ipp.text = String(Int(ippan1))
-        sen.text = String(Int(senmonkamoku))
-        pbll.text = String(Int(pbl))
+        jo.text = "\(String(Int(joron2)))/3"
+        ipp.text = "\(String(Int(ippan1)))/4"
+        sen.text = "\(String(Int(senmonkamoku)))/12"
+        pbll.text = "\(String(Int(pbl)))/2"
+        if(Float(joron2/3)>1){
+            remain = remain + (3-joron2)
+        }
+        if(Float(ippan1/4)>1){
+            remain = remain + (4-ippan1)
+        }
+        if(Float(senmonkamoku/12)>1){
+            remain = remain + (12-senmonkamoku)
+        }
+        if(Float(pbl/2)>1){
+            remain = remain + (2-pbl)
+        }
+        
+        engurah.value = CGFloat(((21-remain)/remain)*100)
           }
 
 
