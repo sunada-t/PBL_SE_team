be3bef74b1ccb675dd84d40d10d861fa16de046b
hiromu-m <v010maaa@gmail.com>
2020-02-16 22:30:03 +0900

    使っているライブラリなどのバージョンを更新したのとグラフ機能を入れた

--- Pods/Target Support Files/Pods-apitest/Pods-apitest-frameworks.sh
+++ Pods/Target Support Files/Pods-apitest/Pods-apitest-frameworks.sh
@@ -163,10 +163,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kanna/Kanna.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MBCircularProgressBar/MBCircularProgressBar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kanna/Kanna.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MBCircularProgressBar/MBCircularProgressBar.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- apitest/NaistWebViewController.swift
+++ apitest/NaistWebViewController.swift
@@ -7,17 +7,46 @@
 //fffsssssssssa
 
 import UIKit
+import MBCircularProgressBar
 
-let ippan1 = Common.getUserDefaults(key: "aaa")
-let joron2 = Common.getUserDefaults(key: "bbb")
-let senmonkamoku = Common.getUserDefaults(key: "ccc")
-let pbl = Common.getUserDefaults(key: "eee")
+
 
 class NaistWebViewController: UIViewController{
+    
+    @IBOutlet weak var engurah: MBCircularProgressBarView!
+    
+    @IBOutlet weak var joron: UIProgressView!
+    @IBOutlet weak var ippan: UIProgressView!
+    @IBOutlet weak var sennmonkuso: UIProgressView!
+    @IBOutlet weak var pblb: UIProgressView!
+    
+    @IBOutlet weak var jo: UILabel!
+    @IBOutlet weak var ipp: UILabel!
+    @IBOutlet weak var sen: UILabel!
+    @IBOutlet weak var pbll: UILabel!
+    
+    
+    
+    
+    let ippan1 = Common.getUserDefaults(key: "aaa")as! Double
+    let joron2 = Common.getUserDefaults(key: "bbb")as! Double
+    let senmonkamoku = Common.getUserDefaults(key: "ccc")as! Double
+    let pbl = Common.getUserDefaults(key: "eee")as! Double
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        engurah.value = 90
+        joron.progress = Float(joron2/3)
+        ippan.progress = Float(ippan1/4)
+        sennmonkuso.progress = Float(senmonkamoku/12)
+        pblb.progress = Float(pbl/2)
+        jo.text = String(Int(joron2))
+        ipp.text = String(Int(ippan1))
+        sen.text = String(Int(senmonkamoku))
+        pbll.text = String(Int(pbl))
           }
 
+
+    
     }
 
