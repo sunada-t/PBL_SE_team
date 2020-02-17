15ba961ad4e385bbce3a0d1895c0597071ce6fc3
hiromu-m <v010maaa@gmail.com>
2020-02-17 13:07:35 +0900

    web ページのUDの空処理，今は一つの項目joronのUDが空なら全部からという前提でやっている

--- apitest/NaistWebViewController.swift
+++ apitest/NaistWebViewController.swift
@@ -26,12 +26,6 @@ class NaistWebViewController: UIViewController{
     @IBOutlet weak var pbll: UILabel!
     @IBOutlet weak var assesment: UILabel!
     
-    @IBOutlet weak var jojo: UIButton!
-
-    @IBOutlet weak var papa: UIButton!
-    @IBOutlet weak var sensen: UIButton!
-    @IBOutlet weak var pbpb: UIButton!
-    
     var ippan1 = 0.0
     var joron2 = 0.0
     var senmonkamoku = 0.0
@@ -45,49 +39,74 @@ class NaistWebViewController: UIViewController{
             senmonkamoku = Common.getUserDefaults(key: "ccc")as! Double
             pbl = Common.getUserDefaults(key: "eee")as! Double
         }
-        
-        
-        
-       
-        
-        joron.progress = Float(joron2/3)
-        ippan.progress = Float(ippan1/4)
-        sennmonkuso.progress = Float(senmonkamoku/12)
-        pblb.progress = Float(pbl/2)
-        
-        jo.text = "\(String(Int(joron2)))/3"
-        ipp.text = "\(String(Int(ippan1)))/4"
-        sen.text = "\(String(Int(senmonkamoku)))/12"
-        pbll.text = "\(String(Int(pbl)))/2"
-        
-        if(Float(joron2/3)<1){
-            remain = remain + (3-joron2)
-        }
-        if(Float(ippan1/4)<1){
-            remain = remain + (4-ippan1)
-        }
-        if(Float(senmonkamoku/12)<1){
-            remain = remain + (12-senmonkamoku)
-        }
-        if(Float(pbl/2)<1){
-            remain = remain + (2-pbl)
-        }
-        
-        engurah.value = CGFloat(Float((21-remain)/21)*100)
-        
-        if(engurah.value>=90){
-            assesment.text = "後は英語だけかな？残り卒論がんばろう！"
-        }else if(engurah.value>=60){
-            assesment.text = "今が頑張りところ"
-        }else if(engurah.value>=30){
-            assesment.text = "M1前半はテストが多いががんばろう"
-        }else if(engurah.value >= 0){
-            assesment.text = "今が冬なら君は詰んでいるよ，潔く諦めよう"
-        }
+        caluunit()
         
           }
+    
+    func caluunit(){
+        
+             joron.progress = Float(joron2/3)
+             ippan.progress = Float(ippan1/4)
+             sennmonkuso.progress = Float(senmonkamoku/12)
+             pblb.progress = Float(pbl/2)
+             
+             jo.text = "\(String(Int(joron2)))/3"
+             ipp.text = "\(String(Int(ippan1)))/4"
+             sen.text = "\(String(Int(senmonkamoku)))/12"
+             pbll.text = "\(String(Int(pbl)))/2"
+             
+             if(Float(joron2/3)<1){
+                 remain = remain + (3-joron2)
+             }
+             if(Float(ippan1/4)<1){
+                 remain = remain + (4-ippan1)
+             }
+             if(Float(senmonkamoku/12)<1){
+                 remain = remain + (12-senmonkamoku)
+             }
+             if(Float(pbl/2)<1){
+                 remain = remain + (2-pbl)
+             }
+             
+             engurah.value = CGFloat(Float((21-remain)/21)*100)
+             
+             if(engurah.value>=90){
+                 assesment.text = "後は英語だけかな？残り卒論がんばろう！"
+             }else if(engurah.value>=60){
+                 assesment.text = "今が頑張りところ"
+             }else if(engurah.value>=30){
+                 assesment.text = "M1前半はテストが多いががんばろう"
+             }else if(engurah.value >= 0){
+                 assesment.text = "今が冬なら君は詰んでいるよ，潔く諦めよう"
+             }
+        
+    }
+    
+    
+    
+    @IBAction func jorob(_ sender: Any) {
+       joron2 = joron2 - 1
+              caluunit()
+    }
+    
 
-
+    @IBAction func ippb(_ sender: Any) {
+        ippan1 = ippan1 - 2
+               caluunit()
+    }
+    
+    
+    @IBAction func semonb(_ sender: Any) {
+       senmonkamoku = senmonkamoku - 2
+               caluunit()
+    }
+    
+    
+    @IBAction func pblbbb(_ sender: Any) {
+        pbl = pbl - 1
+               caluunit()
+    }
+    
     
     }
 
